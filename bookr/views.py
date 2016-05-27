d7e3a6f94da8b67fa4d1c1c115adeda03ba0c1dd
Nicole Peoples <nicolecpeoples@gmail.com>
2016-05-26 17:43:21 -0700

    change in views.py

--- bookr/views.py
+++ bookr/views.py
@@ -90,18 +90,20 @@ class VenueIndex(View):
 		
 
 class Venues(View):
+
+	def get(self,request, venue_id):
+		get_venue = Venue.objects.get(id=venue_id)
+		context= {
+			'get_venue': get_venue
 	def get(self,request):
 		venueInfo = Venue.objects.all()
 		print(venueInfo)
 		context = {
 			'venueInfo': venueInfo,
+
 		}
 		return render(request, 'bookrraven/venue.html', context)
 		
-
-	def getVenueInfo(self, request):
-		# get venue info
-		pass
 
 	def getEventList(self, request):
 		# get event list info
